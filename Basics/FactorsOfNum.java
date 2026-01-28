281c0e2506ae94043635d60ba5d741083420b698
PNKishore01 <nagakishore3639@gmail.com>
2026-01-28 21:48:38 +0530

    Fcators of a given Number

---
+++ Basics/FactorsOfNum.java
@@ -0,0 +1,11 @@
+class FactorsOfNum{
+
+    public static void main(String[]args){
+        int num = 18;
+        System.out.print("Factors of " + num + " is: ");
+        for(int i=1; i<=num/2; i++){
+            if(num%i==0)
+                System.out.print(i + " ");
+        }
+    }
+}
