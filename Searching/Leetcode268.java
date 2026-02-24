e9ce10a598917df91928fddc0d65294afc222908
PNKishore01 <nagakishore3639@gmail.com>
2026-02-24 21:38:31 +0530

    find a missing in array

---
+++ Searching/Leetcode268.java
@@ -0,0 +1,68 @@
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the 
+ * range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the 
+range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation:
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the 
+range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation:
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the 
+range since it does not appear in nums.
+ */
+
+import java.util.*;
+class Leetcode268{
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n value: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        missingNum(arr);
+        missingNum1(arr);
+        sc.close();
+    }
+
+    //Approach-1
+    //System.out.println("Approach - 1");
+    static void missingNum(int[] arr){
+        int n = arr.length;
+        int total = (n*(n+1))/2;
+        for(int i:arr){
+            total -= i;
+        }
+        System.out.println("Missing Number is: " + total);
+    }
+
+    //Approach-2
+    //System.out.println("Approach - 2");
+    static void missingNum1(int[] arr){
+        int n = arr.length;
+        int total = 0;
+        for(int i=1; i<=n; i++){
+            total ^= i;
+        }
+        for(int i:arr){
+            total ^= i;
+        }
+        System.out.println("Missing Number is: " + total);
+    }
+
+}
