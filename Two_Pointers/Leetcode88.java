4d32fd4fab92c7cf1d5d542b9d3bee5060a8d76b
PNKishore01 <nagakishore3639@gmail.com>
2026-03-03 21:36:10 +0530

    Merge Sorted Array

---
+++ Two_Pointers/Leetcode88.java
@@ -0,0 +1,44 @@
+/**
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can 
+fit in nums1.
+ */
+
+import java.util.*;
+class Leetcode88{
+
+    public static void main(String[]args){
+        int[] arr1 = {1,2,3,0,0,0};
+        int[] arr2 = {2,5,6};
+        merge(arr1, 3, arr2, 3);
+        System.out.println(Arrays.toString(arr1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=m;
+        if(n!=0){
+            for(int i:nums2){
+                nums1[j++]=i;
+            }
+        }
+        else if(m==0 && n>0)
+            nums1[0]=nums2[0];
+        Arrays.sort(nums1);
+    }
+}
