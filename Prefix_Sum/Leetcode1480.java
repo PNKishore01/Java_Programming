da371614479c7cd44737ac119da4365213938657
PNKishore01 <nagakishore3639@gmail.com>
2026-03-26 12:08:30 +0530

    Running sum of 1d array

---
+++ Prefix_Sum/Leetcode1480.java
@@ -0,0 +1,43 @@
+/**
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ */
+
+import java.util.*;
+class Leetcode1480{
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row count value: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(runningSum(arr)));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int n=nums.length;
+        int[] arr = new int[n];
+        arr[0]=nums[0];
+        for(int i=1; i<n; i++){
+            arr[i]=nums[i]+arr[i-1];
+        }
+        return arr;
+    }
+}
